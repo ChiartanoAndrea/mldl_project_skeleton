a26e0dcea09ba9282d30fd0de966b20749fc63d0
ANDREA CHIARTANO s343401 <s343401@studenti.polito.it>
2025-11-04 17:10:57 +0100

    add giusta(forse)

--- mldl_lab02.py
+++ mldl_lab02.py
@@ -101,61 +101,8 @@ class CustomNet(nn.Module):
 
         return x
 
-def train(epoch, model, train_loader, criterion, optimizer):
-    model.train()
-    running_loss = 0.0
-    correct = 0
-    total = 0
-
-
-    for batch_idx, (inputs, targets) in enumerate(train_loader):
-        inputs, targets = inputs.cuda(), targets.cuda()
-
-        # Prediction
-
-        pred=model(inputs)
-        loss=criterion(pred,targets)
-
-        # Backward
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        running_loss += loss.item()
-        _, predicted = pred.max(1)
-        total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
-
-    train_loss = running_loss / len(train_loader)
-    train_accuracy = 100. * correct / total
-    print(f'Train Epoch: {epoch} Loss: {train_loss:.6f} Acc: {train_accuracy:.2f}%')
 
 # Validation loop
-def validate(model, val_loader, criterion):
-    model.eval()
-    val_loss = 0
-
-    correct, total = 0, 0
-
-    with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(val_loader):
-            inputs, targets = inputs.cuda(), targets.cuda()
-
-            # todo...
-            pred= model(inputs)
-            test_loss= criterion(pred,targets)
-
-
-            val_loss += test_loss.item()
-            _, predicted = pred.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
-
-    val_loss = val_loss / len(val_loader)
-    val_accuracy = 100. * correct / total
-
-    print(f'Validation Loss: {val_loss:.6f} Acc: {val_accuracy:.2f}%')
-    return val_accuracy
 
 """## Putting everything together"""
 
---
+++ train.py
@@ -0,0 +1,15 @@
+from models.models import models 
+
+model = CustomNet().cuda()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+best_acc = 0
+
+# Run the training process for {num_epochs} epochs
+num_epochs = 10
+for epoch in range(1, num_epochs + 1):
+    train(epoch, model, train_loader, criterion, optimizer)
+
+
+#print(f'Best validation accuracy: {best_acc:.2f}%')
